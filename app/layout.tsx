aa153bd7376511790c486b01e84c5f2797c5e6b9
Fabien KORGO <kograss20@gmail.com>
2024-12-20 13:04:25 +0100

    Google tag manager installation

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,31 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" data-theme="black" className={"text-white"}>
+      <head>
+        {/* Google Tag Manager */}
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-5C6XRV6Q');
+            `,
+          }}
+        />
+      </head>
       <body>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5C6XRV6Q"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         <Script
           id="tawkto-script"
           strategy="afterInteractive"
